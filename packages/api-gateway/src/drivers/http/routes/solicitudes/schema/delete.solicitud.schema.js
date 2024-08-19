ffe110278cad4bd14e6ba7c6b801be173672c1bc
Dylan Sanchez <dylansanchez2902@gmail.com>
2024-08-19 10:42:03 -0600

    Respuesta del schema

--- packages/api-gateway/src/drivers/http/routes/solicitudes/schema/delete.solicitud.schema.js
+++ packages/api-gateway/src/drivers/http/routes/solicitudes/schema/delete.solicitud.schema.js
@@ -1,10 +1,6 @@
-const { solicitud } = require('./properties/solicitud');
-const { persona } = require('../../usuarios/schema/properties/persona');
-const { responseProperties } = require('./properties/responseProperties');
-
 const deleteAlumnoSchema = {
   tags: ['Solicitudes'],
-  description: 'Given an SolicitudId delete an Solicitud in data base',
+  description: 'Given an SolicitudId delete an Solicitud in the database',
   params: {
     title: 'delete solicitud',
     type: 'object',
@@ -17,22 +13,8 @@ const deleteAlumnoSchema = {
     200: {
       type: 'object',
       properties: {
-        data: {
-          type: 'object',
-          properties: {
-            id: { type: 'integer' },
-            ...solicitud,
-            ...responseProperties,
-            persona: {
-              type: 'object',
-              properties: {
-                id: { type: 'integer' },
-                ...persona,
-                ...responseProperties,
-              },
-            },
-          },
-        },
+        success: { type: 'boolean', example: true },
+        message: { type: 'string', example: 'Solicitud eliminada correctamente' },
       },
     },
   },
